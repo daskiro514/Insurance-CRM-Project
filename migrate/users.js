38a0ec89c2eed80a4f2c051c925836ee98b0e792
daskiro514 <luckybuzstar@gmail.com>
2021-09-14 21:32:06 +0900

    update

--- migrate/users.js
+++ migrate/users.js
@@ -5,14 +5,27 @@ const salt = bcrypt.genSaltSync(10);
 const normalize = require('normalize-url')
 const gravatar = require('gravatar')
 
+// FOR LOCAL
 const users = [
   {
     type: 'admin',
-    name: 'Steven Hooley',
-    email: 'sbhooley@gmail.com',
-    avatar: normalize(gravatar.url('sbhooley@gmail.com', { s: '200', r: 'pg', d: 'mm' }), { forceHttps: true }),
+    name: 'Daskiro King',
+    email: 'progdev77@gmail.com',
+    avatar: normalize(gravatar.url('progdev77@gmail.com', { s: '200', r: 'pg', d: 'mm' }), { forceHttps: true }),
     password: bcrypt.hashSync('admin123', salt),
   }
 ]
+
+
+// // FOR SERVER
+// const users = [
+//   {
+//     type: 'admin',
+//     name: 'Steven Hooley',
+//     email: 'sbhooley@gmail.com',
+//     avatar: normalize(gravatar.url('sbhooley@gmail.com', { s: '200', r: 'pg', d: 'mm' }), { forceHttps: true }),
+//     password: bcrypt.hashSync('admin123', salt),
+//   }
+// ]
 
 module.exports = users;
