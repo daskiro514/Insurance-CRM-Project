79fb5e135ac40e5b90e20800b8555e5a44e792e0
daskiro514 <luckybuzstar@gmail.com>
2021-09-24 02:32:02 +0900

    updated

--- client/src/actions/customer.js
+++ client/src/actions/customer.js
@@ -1,11 +1,13 @@
 import api from '../utils/api'
 import { setAlert } from './alert'
 import { goPage } from './admin'
+import { loadUser } from './auth'
 
 export const updateOnCustomer = (formData, history, customerID) => async dispatch => {
   const response = await api.post(`/customer/updateOnCustomer/${customerID}`, formData)
 
   if (response.data.success) {
+    dispatch(loadUser())
     dispatch(setAlert('Updated Successfully!', 'success'))
     dispatch(goPage(history, ''))
   }
